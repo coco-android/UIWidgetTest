5b9dd63965240464c27783299ada4b9d87aa3d74
wangzz <81303012@qq.com>
2019-12-13 16:19:37 +0800

    引入包

---
+++ app/src/main/java/com/coco/study/uiwidgettest/PercentFrameLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.coco.study.uiwidgettest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class
+PercentFrameLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_frame_layout);
+    }
+}
