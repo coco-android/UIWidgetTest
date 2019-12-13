4af8f1a05d299e03e9a92269712a4e5aaf928944
wangzz <81303012@qq.com>
2019-12-13 15:09:37 +0800

    布局

---
+++ app/src/main/java/com/coco/study/uiwidgettest/LinearLayout/LinearLayoutActivity.java
@@ -0,0 +1,27 @@
+package com.coco.study.uiwidgettest.LinearLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.coco.study.uiwidgettest.R;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+
+        // layout_weight
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LinearLayoutActivity.this, LayoutWeightActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/coco/study/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/coco/study/uiwidgettest/MainActivity.java
@@ -3,9 +3,11 @@ package com.coco.study.uiwidgettest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import com.coco.study.uiwidgettest.LinearLayout.LinearLayoutActivity;
 import com.coco.study.uiwidgettest.common.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -91,6 +93,16 @@ public class MainActivity extends BaseActivity {
                 progressDialog.setMessage("注意：ProgressDialog已经废弃了！");
                 progressDialog.setCancelable(true);
                 progressDialog.show();
+            }
+        });
+
+        // 5. LinearLayout 布局
+        Button linearLayoutBtn = findViewById(R.id.linear_layout);
+        linearLayoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
             }
         });
 
